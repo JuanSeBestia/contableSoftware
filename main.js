b67cae956a73a03bc521462c250a25e9c22b42bb
SeiferDisast <seifer.cloud@gmail.com>
2023-05-22 19:36:48 -0500

    fix loggin

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-const userLogin = document.querySelector('.userLogin');
-const passwordLogin = document.querySelector('.passwordLogin');
-const buttonLogin = document.querySelector('.submitLogin');
+const userLogin = document.querySelector('#userLogin');
+const passwordLogin = document.querySelector('#passwordLogin');
+const buttonLogin = document.querySelector('#submitLogin');
+const form1 = document.querySelector('#form');
 
 let accesToken = false;
 let mockapUser = "admin";
 let mockapPassword = "Admin123";
 
-buttonLogin.addEventListener('click', () => {
-    if (loginValidate()) {
-        window.location.replace("mainPage.html");
-    }
-});
+form1.addEventListener('submit', loginValidate)
 
-function loginValidate() {
+function loginValidate(event) {
     try {
+        event.preventDefault();
         const loginVal = userLogin.value;
         const passVal = passwordLogin.value;
 
         if (loginVal == mockapUser && passVal == mockapPassword) {
-            return true;
+            window.location.replace("mainPage.html");
         } else {
-            return false;
+            alert("Login invalid");
         }
     } catch (error) {
         console.error("An error occurred during login validation:", error);
